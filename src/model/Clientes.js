fdc4ec5f6c5bd10483e8a635250965206d478d82
wagnermarques <wagnerdocri@gmail.com>
2026-04-22 22:34:00 -0300

    Primeiro Commit

---
+++ src/model/Clientes.js
@@ -0,0 +1,10 @@
+class Cliente{
+    constructor(id, nome, telefone, endereco){
+        this.id = this.id;
+        this.nome = nome;
+        this.telefone = telefone;
+        this.endereco = endereco;
+    }
+}
+
+module.exports = Cliente;
